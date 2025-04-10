8289568bd3bea831f0dbc52f54c7b0fc00e6a5b0
joneikholmkea <jone@kea.dk>
2025-04-10 09:48:03 +0200

    ball is reflected + bat

--- App.js
+++ App.js
@@ -38,10 +38,54 @@ function GameComponent(){
     }
   }
 
+  const bat = {
+    position:{
+      x: width / 2 - 25,
+      y: height
+    },
+    size: 100,
+    renderer: (props)=>{
+      const {position, size} = props
+      return(
+        <View
+        style={{
+          backgroundColor: 'green',
+          position: 'absolute',
+          left: position.x,
+          top: height - 20,
+          width: size,
+          height: size/5,
+          borderRadius: size / 2
+        }}
+        >
+        </View>
+      )
+    }
+  }
+
+
   function update(entities, { time }){
       const ballEntity = entities.ball
+
       ballEntity.position.x += ballEntity.velocity.x * time.delta
       ballEntity.position.y += ballEntity.velocity.y * time.delta
+    // højre side
+      if(ballEntity.position.x + ballEntity.size > width){
+        ballEntity.velocity.x = -1 * Math.abs(ballEntity.velocity.x)
+      }
+      // venstre side
+      if(ballEntity.position.x < 0){
+        ballEntity.velocity.x = Math.abs(ballEntity.velocity.x)
+      }
+      // bund
+      if(ballEntity.position.y + ballEntity.size > height){
+        ballEntity.velocity.y = -1 * Math.abs(ballEntity.velocity.y)
+      }
+       // top
+       if(ballEntity.position.y < 0){
+        ballEntity.velocity.y = Math.abs(ballEntity.velocity.y)
+      }
+
       return entities
   }
 
@@ -49,7 +93,7 @@ function GameComponent(){
     <View style={[{flex:1}, {paddingBottom:insets.bottom}]}>
       <GameEngine
         running={running}
-        entities={{ball}}
+        entities={{ball, bat}}
         systems={[update]}
         style={{flex:1, backgroundColor:'white'}}
       />
