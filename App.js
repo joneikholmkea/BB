b1a7e0ffab81796583b8ad96fd9e986aee0a5aef
joneikholmkea <jone@kea.dk>
2025-04-10 10:29:00 +0200

    added sensor to control bat

--- App.js
+++ App.js
@@ -1,14 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
+import { use, useEffect, useState } from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import { GameEngine} from 'react-native-game-engine'
 import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
+import { DeviceMotion } from 'expo-sensors';
 
 function GameComponent(){
   const [running, setRunning] = useState(true)
+  const [motionData, setMotionData] = useState(null)
+  const [isMotionAvailable, setIsMotionAvailable] = useState(false)
   const insets = useSafeAreaInsets()
   const {width, height} =  Dimensions.get('window')
   
+  useEffect(()=>{
+    async function subscribe() {
+      const available = await DeviceMotion.isAvailableAsync()
+      setIsMotionAvailable(available) // dette tager noget tid...
+      if(available){
+        DeviceMotion.setUpdateInterval(20) // 20 millisekunder imellem updates
+        DeviceMotion.addListener(deviceMotionData =>{
+          setMotionData(deviceMotionData)
+          //console.log(deviceMotionData)
+        })
+      }else{
+        console.log("ikke adgang til sensor")
+      }
+    }
+    subscribe()
+    return ()=>{
+      DeviceMotion.removeAllListeners()
+    }
+  },[])
+
+
   const ball = {
     position:{
       x: width / 2 - 25,
@@ -62,11 +86,12 @@ function GameComponent(){
       )
     }
   }
+// Opgave: få bolden til at bounce på bat, ellers falde ned og spillet stopper.
 
 
   function update(entities, { time }){
       const ballEntity = entities.ball
-
+      const batEntity = entities.bat
       ballEntity.position.x += ballEntity.velocity.x * time.delta
       ballEntity.position.y += ballEntity.velocity.y * time.delta
     // højre side
@@ -84,6 +109,15 @@ function GameComponent(){
        // top
        if(ballEntity.position.y < 0){
         ballEntity.velocity.y = Math.abs(ballEntity.velocity.y)
+      }
+
+      // flyt bat
+      let newPos = 100
+      if(isMotionAvailable && motionData){
+        newPos = 250 * motionData.rotation.gamma + 150  // gamma giver -1 ... +1
+      }
+      if(!isNaN(newPos)){
+        batEntity.position.x = newPos
       }
 
       return entities
