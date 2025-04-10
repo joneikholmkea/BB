06278de30615fb8ec51689971d110cb88d8e7a7a
joneikholmkea <jone@kea.dk>
2025-04-10 09:16:17 +0200

    basic BB

--- App.js
+++ App.js
@@ -1,12 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { GameEngine} from 'react-native-game-engine'
+import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
+
+function GameComponent(){
+  const [running, setRunning] = useState(true)
+  const insets = useSafeAreaInsets()
+  const {width, height} =  Dimensions.get('window')
+  
+  const ball = {
+    position:{
+      x: width / 2 - 25,
+      y: height / 2
+    },
+    size: 50,
+    velocity:{
+      x: 0.1,
+      y: 0.1
+    },
+    renderer: (props)=>{
+      const {position, size} = props
+      return(
+        <View
+        style={{
+          backgroundColor: 'red',
+          position: 'absolute',
+          left: position.x,
+          top: position.y,
+          width: size,
+          height: size,
+          borderRadius: size / 2
+        }}
+        >
+        </View>
+      )
+    }
+  }
+
+  function update(entities, { time }){
+      const ballEntity = entities.ball
+      ballEntity.position.x += ballEntity.velocity.x * time.delta
+      ballEntity.position.y += ballEntity.velocity.y * time.delta
+      return entities
+  }
+
+  return (
+    <View style={[{flex:1}, {paddingBottom:insets.bottom}]}>
+      <GameEngine
+        running={running}
+        entities={{ball}}
+        systems={[update]}
+        style={{flex:1, backgroundColor:'white'}}
+      />
+    </View>
+  )
+
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <GameComponent />
+    </SafeAreaProvider>
   );
 }
 
